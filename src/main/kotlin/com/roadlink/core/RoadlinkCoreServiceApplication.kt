b0eb11ce91ae360a325e7bd288d31ec917c7de08
jorgejcabrera <cabrerajjorge@gmail.com>
2023-09-17 12:11:19 -0300

    initial commit

---
+++ src/main/kotlin/com/roadlink/core/RoadlinkCoreServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.roadlink.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadlinkCoreServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoadlinkCoreServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/roadlink/core/RoadlinkCoreServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roadlink.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoadlinkCoreServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
