604c01d76a009f30430b5989d20543f0eddc3b8d
jorgecabrera <cabrerajjorge@gmail.com>
2023-10-08 13:59:03 -0300

    exception handler advice

---
+++ api/src/main/kotlin/com/roadlink/core/api/error/ExceptionHandlerController.kt
@@ -0,0 +1,71 @@
+package com.roadlink.core.api.error
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class ExceptionHandlerController {
+    @ExceptionHandler(
+        MethodArgumentNotValidException::class,
+    )
+    fun handleInvalidArgument(ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        val message = ex.allErrors.map {
+            it.defaultMessage
+        }.sortedBy {
+            it
+        }.joinToString(", ")
+
+        val errorMessage = ErrorResponse(
+            HttpStatus.BAD_REQUEST.toString(), message = message
+        )
+
+        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(
+        HttpMessageNotReadableException::class
+    )
+    fun handleInvalidJSON(ex: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
+        val errorMessage = ErrorResponse(
+            HttpStatus.BAD_REQUEST.toString(), message = "Invalid request format: could not be parsed to a valid JSON"
+        )
+
+        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleMethodNotSupportedException(ex: HttpRequestMethodNotSupportedException): ResponseEntity<ErrorResponse> {
+        val errorMessage = ErrorResponse(
+            HttpStatus.METHOD_NOT_ALLOWED.toString(), message = ex.message ?: "Method not allowed"
+        )
+
+        return ResponseEntity(errorMessage, HttpStatus.METHOD_NOT_ALLOWED)
+    }
+
+    @ExceptionHandler
+    fun handleAllUncaughtException(ex: Throwable): ResponseEntity<ErrorResponse> {
+        LOGGER.error("Unexpected exception:", ex)
+
+        val errorMessage = ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.toString(), message = "Oops, something wrong happened"
+        )
+
+        return ResponseEntity(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(this::class.java)
+    }
+}
+
+class ErrorResponse(
+    val code: String,
+    val message: String
+)
