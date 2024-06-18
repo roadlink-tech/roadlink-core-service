8657d5b90e606eacd33f93904121f101b03b1761
mbosch <mbosch@whykeyway.com>
2024-06-17 21:40:44 -0300

    chore: friendship status yourself documentation

--- domain/src/main/kotlin/com/roadlink/core/domain/friend/FriendshipStatus.kt
+++ domain/src/main/kotlin/com/roadlink/core/domain/friend/FriendshipStatus.kt
@@ -5,5 +5,9 @@ enum class FriendshipStatus {
     NOT_FRIEND,
     PENDING_FRIENDSHIP_SOLICITUDE_SENT,
     PENDING_FRIENDSHIP_SOLICITUDE_RECEIVED,
+    /**
+     * This result indicates that the calculation of friendship status
+     * involves the same user for both comparison roles
+     */
     YOURSELF,
 }
